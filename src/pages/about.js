7bfe251f72c50a81c55f14289295f95caf103edb
wesin ribeiro <wesinec@gmail.com>
2020-03-25 21:22:58 -0300

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,13 @@ const AboutPage = () => (
     <Layout>
     <SEO title="About" />
     <h1>About</h1>
+    <p>
+    I received bachelor degree in Computer Engineering (2007) and M. Sc. (2011) from 
+    the Universidade Federal do Pará, Brazil. Currently, I am a Ph. D. candidate
+    under advisement of Prof. Dr. Aldebaro Klautau at the Universidade Federal do Pará, Brazil. I have experience 
+    in research and development projects related to deep learning and data analysis. 
+    systems. My topics of interest are software engineering, data science, and artificial neural networks.
+    </p>
     <ul>
         <li>
             <Link to="/" activeStyle={{ color: "blue"}}>Home</Link>
