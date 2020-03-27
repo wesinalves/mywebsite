72d0dec8c99bea801557ac94b4edd4795b8cbc04
wesin ribeiro <wesinec@gmail.com>
2020-03-27 15:35:17 -0300

    udpate layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -8,18 +8,31 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import "./layout.css"
 import Profile from "../Profile"
+
+import styled from 'styled-components'
+
+const LayoutWrapper = styled.section`
+    display: flex;
+`
+
+const LayoutMain = styled.main`
+    background: #16202c;
+    min-height: 100vh;
+    padding: 0 3.75rem 0 20rem;
+    transition: background, color 0.5s;
+    width: 100%;
+`
 
 const Layout = ({ children }) => {
 
     return (
-        <>
+        <LayoutWrapper>
             <aside>
                 <Profile/>
             </aside>
-            <main>{children}</main>
-        </>
+            <LayoutMain>{children}</LayoutMain>
+        </LayoutWrapper>
     )
 }
 
