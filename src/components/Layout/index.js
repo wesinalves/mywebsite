ff9cf392143060b00d09f6aa7679c76127f5a8d7
wesin ribeiro <wesinec@gmail.com>
2020-03-29 23:27:48 -0300

    add side bar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Profile from "../Profile"
+import Sidebar from "../Sidebar"
 
 
 import GlobalStyles from '../../styles/global'
@@ -21,9 +21,7 @@ const Layout = ({ children }) => {
     return (
         <S.LayoutWrapper>
             <GlobalStyles />
-            <aside>
-                <Profile/>
-            </aside>
+            <Sidebar />
             <S.LayoutMain>{children}</S.LayoutMain>
         </S.LayoutWrapper>
     )
--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -7,7 +7,7 @@ export const LayoutWrapper = styled.section`
 export const LayoutMain = styled.main`
     background: #16202c;
     min-height: 100vh;
-    padding: 0 3.75rem 0 3.75rem;
+    padding: 0 3.75rem 0 20rem;
     transition: background, color 0.5s;
     width: 100%;
 `
